a23bc4abd192f7605eb401e28fae88e992633719
jchen-vercel <jason.chen@vercel.com>
2022-07-05 10:33:07 -0400

    css commits

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import styles from "../styles/About.module.css";
+
+function About() {
+  return <div className={styles.highlight}> About Page </div>;
+}
+
+export default About;
---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Contact.module.css";
+
+function Contact() {
+  return <div class={styles.highlight}> Contact Page</div>;
+}
+
+export default Contact;
